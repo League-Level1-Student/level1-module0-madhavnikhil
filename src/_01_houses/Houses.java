88331d01a2671b7d6d0bdca69acf3e1e4e95b6f9
student <student@ip-172-31-62-117.us-west-2.compute.internal>
2021-03-04 16:56:43 -0800

    houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,30 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	Robot Lupin = new Robot();
 	public void run() {
+		for (int i = 0; i < 11; i++) {
+			drawHouse();	
+		}
 		
+		
+	}
+	
+	void drawHouse() {
+		
+		Lupin.penDown();
+		Lupin.move(100);
+		Lupin.turn(90);
+		Lupin.move(50);
+		Lupin.turn(90);
+		Lupin.move(100);
+		Lupin.turn(-90);
+		Lupin.move(30);
+		Lupin.turn(-90);
+		
+	
+	
 	}
 }
