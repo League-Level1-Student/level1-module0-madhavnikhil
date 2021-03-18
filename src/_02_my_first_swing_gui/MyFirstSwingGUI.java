fba3e4e1055e9ea3f4ae18bf6c725c4fc3d834c3
student <student@ip-172-31-62-197.us-west-2.compute.internal>
2021-03-18 16:50:59 -0700

    MyFirstSwing

--- src/_02_my_first_swing_gui/MyFirstSwingGUI.java
+++ src/_02_my_first_swing_gui/MyFirstSwingGUI.java
@@ -16,24 +16,24 @@ public class MyFirstSwingGUI {
 	public void run() {
 
 		// 1. Declare and initialize an object of the JFrame class
-
+JFrame Window = new JFrame();
 		// 2. Set your JFrame object to be visible
-
+Window.setVisible(true);
 		// 3. Open MyFirstSwingGUIRunner and run your program. Do you see your window? It's probably very
 		// small.
-
+Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 4. Use your JFrame to call setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
 
 		// 5. Declare and initialize an object of the JPanel class
-
+JPanel Dumbledore = new JPanel();
 		// 6. Declare and initialize an object of the JLabel class
-
-		// 7. Set the text of the JLabel to a lovely greeting
-
+JLabel Hagrid = new JLabel();
+// 7. Set the text of the JLabel to a lovely greeting
+Hagrid.setText("Expecto Patronum");
 		// 8. Add the JPanel object to the JFrame
-
+Window.add(Dumbledore);
 		// 9. Add the JLabel object to the JPanel
-
+Dumbledore.add(Hagrid);
 		// 10. Pack your JFrame
 
 		// 11. Run your program again. Do you see your message?
@@ -50,7 +50,7 @@ public class MyFirstSwingGUI {
 		try {
 			return new ImageIcon(ImageIO.read(new MyFirstSwingGUI().getClass().getResourceAsStream("java.png")));
 		} catch (IOException e) {
-			e.printStackTrace()
+			e.printStackTrace();
 			return null;
 		}
 	}
