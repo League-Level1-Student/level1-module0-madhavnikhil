b329fcab50c3833c03250c2b13a638be97e2f694
student <student@ip-172-31-56-1.us-west-2.compute.internal>
2021-07-01 16:55:18 -0700

    fortune cookies

---
+++ src/_07_fortune_cookie/fortunecookies.java
@@ -0,0 +1,33 @@
+package _07_fortune_cookie;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class fortunecookies {
+	        public void showButton() {
+	            
+	    JFrame Window = new JFrame(); 
+	    		
+	    Window.setVisible(true);
+	    		
+	    Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  
+	    	
+	    JPanel Dumbledore = new JPanel();
+	      
+	    JButton button = new JButton();
+	    Dumbledore.add(button);
+	    Window.pack();
+	        	
+	        }
+	        {  JButton button = new JButton();
+	       Window.add(button);
+	       Window.pack();
+}
+	        
+	        
+}
+	    
+
+
