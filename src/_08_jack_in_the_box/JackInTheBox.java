3aaa71648828c9c1d08ad92e5dbe0ce9c300918b
student <student@ip-172-31-62-190.us-west-2.compute.internal>
2021-07-08 16:49:11 -0700

    jackinthebox

---
+++ src/_08_jack_in_the_box/JackInTheBox.java
@@ -0,0 +1,42 @@
+package _08_jack_in_the_box;
+
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class JackInTheBox {
+
+	
+	
+private void showPicture(String fileName) { 
+    try {
+        JLabel imageLabel = createLabelImage(fileName);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(imageLabel);
+        frame.setVisible(true);
+        frame.pack();
+   } catch (Exception e) {
+        e.printStackTrace();
+   }
+
+}
+private JLabel createLabelImage(String fileName) {
+   try {
+        URL imageURL = getClass().getResource(fileName);
+        if (imageURL == null) {
+             System.err.println("Could not find image " + fileName);
+             return new JLabel();
+        } else {
+             Icon icon = new ImageIcon(imageURL);
+             JLabel imageLabel = new JLabel(icon);
+             return imageLabel;
+        }
+   } catch (Exception e) {
+        System.err.println("Could not find image " + fileName);
+        return new JLabel();
+   }
+}
